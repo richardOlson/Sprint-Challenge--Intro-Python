968ca6408a03c189bb73bbba22fde99abc442fe8
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-07-17 19:51:48 -0600

    finish oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+# vehicle is the base class
+class Vehicle:  # vehicle is the base class
+    pass
+
+class GroundVehicle(Vehicle): # GroundVehicle is a child class of Vehicle
+    pass
+
+class Car(GroundVehicle): # car is a child class of GroundVehicle
+    pass
+
+class Motorcycle(GroundVehicle): # Motorcycle is a child class of GroundVehicle
+    pass
+
+class FlightVehicle(Vehicle): #FlightVehicle is a child class of the Vehicle class
+    pass
+
+class Airplane(FlightVehicle): # Airplane is a child class of the class FlightVehicle
+    pass
+
+class Starship(FlightVehicle): # Starship is a child class of the class FlightVehicle
+    pass
